c517eb3fa6655dae92d38792d44e5118c2cf40c7
rheber <rheber@rediffmail.com>
2015-01-20 13:23:22 +1100

    start of parser

--- define.py
+++ define.py
@@ -1,14 +1,25 @@
-import sys
+from html.parser import HTMLParser
+from sys import stderr
 from urllib.request import urlopen
 
 URL = "http://dictionary.reference.com/browse/{word}?s=t"
 DEFINITION_CLASS = "def-content"
+
+
+
+class GlossParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        if ("class", DEFINITION_CLASS) in attrs:
+            print(self.get_starttag_text())
+
 
 def glosses(word):
     """Return the definitions of a word."""
 
     try:
         response = urlopen(URL.format(word=word))
+        parser = GlossParser()
+        parser.feed(response.read())
+        # parser.glosses
     except URLError:
-        print("Error: Couldn't access site", file=sys.stderr)
-    
+        print("Error: Couldn't access site", file=stderr)
